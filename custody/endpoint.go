6464dafbd5e169d5b862767cd2cde156a84d3465
Ase Deliri <deliri.ase@gmail.com>
2026-07-15 23:53:13 -0400

    Own Witness custody endpoint paths

---
+++ custody/endpoint.go
@@ -0,0 +1,9 @@
+package custody
+
+import "github.com/offGridSoft/foundation/v2026/core"
+
+const (
+	OffgridWitnessCustodyRootPath     = "/" + core.ContractVersionToken + "/" + core.ProductTokenWitness + "/custody"
+	OffgridWitnessCustodyOpenPath     = OffgridWitnessCustodyRootPath + "/open"
+	OffgridWitnessCustodyFinalizePath = OffgridWitnessCustodyRootPath + "/finalize"
+)
---
+++ custody/endpoint_test.go
@@ -0,0 +1,29 @@
+package custody
+
+import (
+	"testing"
+
+	"github.com/offGridSoft/foundation/v2026/core"
+)
+
+func TestWitnessCustodyEndpointContractsTable(t *testing.T) {
+	t.Parallel()
+	wantRoot := "/" + core.ContractVersionToken + "/" + core.ProductTokenWitness + "/custody"
+	tests := []struct {
+		name string
+		got  string
+		want string
+	}{
+		{name: "root", got: OffgridWitnessCustodyRootPath, want: wantRoot},
+		{name: "open", got: OffgridWitnessCustodyOpenPath, want: wantRoot + "/open"},
+		{name: "finalize", got: OffgridWitnessCustodyFinalizePath, want: wantRoot + "/finalize"},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			if tc.got != tc.want {
+				t.Fatalf("endpoint = %q, want %q", tc.got, tc.want)
+			}
+		})
+	}
+}
